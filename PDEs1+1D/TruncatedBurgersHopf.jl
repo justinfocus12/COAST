8292953a3a2df5770bc1c26ea1dba08403cbea4f
Justin Finkel <justinfocus12@gmail.com>
2026-06-15 00:47:54 -0500

    various cutoff wavenumbers simulated. Next do an ensemble

--- PDEs1+1D/TruncatedBurgersHopf.jl
+++ PDEs1+1D/TruncatedBurgersHopf.jl
@@ -4,21 +4,21 @@ import Random
 using JLD2: jldopen
 using Infiltrator: @infiltrate
 
-function tendency!(dt_u_s, usq_s, u_s, kmax)
-    usq_s .= 0
-    dt_u_s .= 0
-    # Fill in the usq_s array
-    usq_s[1] = abs2(u_s[1]) + 4*sum(abs2.(u_s[2:kmax+1]))
+function tendency!(dtu, usq, u, kmax)
+    usq .= 0
+    dtu .= 0
+    # Fill in the usq array
+    usq[1] = abs2(u[1]) + 4*sum(abs2.(u[2:kmax+1]))
     for k = 1:kmax
         for m = 1:k-1
-            usq_s[k+1] += 2 * u_s[m+1] * u_s[(k-m)+1]
+            usq[k+1] += 2 * u[m+1] * u[(k-m)+1]
         end
-        usq_s[1] += 2 * u_s[1] * u_s[k+1]
+        usq[k+1] += 2 * u[1] * u[k+1]
         for m = (k+1):kmax
-            usq_s[k+1] += 2 * u_s[m+1] * conj(u_s[(m-k)+1])
+            usq[k+1] += 2 * u[m+1] * conj(u[(m-k)+1])
         end
     end
-    dt_u_s .= (-1im/2) .* (0:kmax) .* usq_s
+    dtu .= (-1im/2) .* (0:kmax) .* usq
     return
 end
 
@@ -53,11 +53,11 @@ function integrate_tbh(u_init, t_init, dt, Nt)
     kmax = length(u_init) - 1
     (
      u_old,u_new,
-     dt_u,usq,
      # scratch for RK4
+     usq,
      urk1,urk2,urk3,urk4,
      dturk1,dturk2,dturk3,dturk4
-    ) = ntuple(_->zeros(Complex{NF}, (kmax+1,)), 12)
+    ) = ntuple(_->zeros(Complex{NF}, (kmax+1,)), 11)
     uhist = zeros(Complex{NF}, (kmax+1, Nt)) # u in spectral space 
     thist = t_init .+ (0:Nt-1).*dt
     # ------- Initialize -------
@@ -104,46 +104,45 @@ end
 
     
 
-function plot_tbh(uhist, t0, dt)
+function plot_tbh_hov_trace(uhist, thist, outfilename)
     Nk,Nt = size(uhist)
     Nx = 251
     ughist = spec2grid(uhist, Nx)
     uglimits = [-1,1].*maximum(abs.(ughist))
     Rlimits = uglimits
     xs = collect(range(0, 2pi; length=Nx))
-    ts = t0 .+ dt.*collect(0:Nt-1)
 
     # Trace out an observable 
     x_target = pi
     ix = argmin(abs.(xs .- x_target)) # location where the observable is measured 
     Roft = ughist[ix,:]
 
-
     thmax = theme_ax()
     xlimits = (0, 2pi)
-    tlimits = (ts[1], ts[end])
-    fig = Figure(size=(400,300))
+    dt = thist[2]-thist[1]
+    tlimits = (thist[1]-dt/2, thist[end]+dt/2)
+    fig = Figure(size=(600,300))
     lout = fig[1,1] = GridLayout()
 
     axhov = Axis(lout[1,1]; thmax..., xticklabelsvisible=false, ylabel="𝑥", yticks=([0,pi,2pi], ["0","π", "2π"]), limits=(tlimits,xlimits), title="𝑢(𝑥,𝑡)")
-    heatmap!(axhov, ts, xs, ughist'; colormap=:coolwarm, colorrange=uglimits)
+    heatmap!(axhov, thist, xs, ughist'; colormap=:coolwarm, colorrange=uglimits)
     hlines!(axhov, xs[ix]; color=:black, linestyle=(:dash,:dense))
 
     Rtickvalues = [Rlimits[1], 0, Rlimits[2]]
     Rticklabels = (u->@sprintf("%.1f", u)).(Rtickvalues)
-    axRoft = Axis(lout[2,1]; thmax..., title="𝑅(𝑡)", yticklabelsvisible=false, ylabelvisible=false, limits=(tuple(Rlimits...), tlimits), xticks=(Rtickvalues, Rticklabels), xticklabelrotation=-pi/2)
-    lines!(axRoft, Roft, ts; color=:black)
-
-
-
-    linkyaxes!(axhov, axRoft)
-    colsize!(lout, 1, Relative(4/5))
-    colgap!(lout, 1, 10)
-
-    save("ughist.png", fig)
+    axRoft = Axis(lout[2,1]; thmax..., ylabel="𝑅(𝑡)", xlabel="𝑡", limits=(tlimits, tuple(Rlimits...)), yticks=(Rtickvalues, Rticklabels), )
+    lines!(axRoft, thist, Roft; color=:black)
+    linkxaxes!(axhov, axRoft)
+    rowsize!(lout, 1, Relative(4/5))
+    rowgap!(lout, 1, 10)
+    save(outfilename, fig)
 end
 
 function main()
+    todo = Dict{String,Bool}(
+                             "spinup" =>           1,
+                             "plot_spinup" =>      1,
+                            )
     # ---------------- Output directories -----------
     dirout_base = "/Users/justinfinkel/Documents/postdoc_mit/computing/COAST_results/PDEs1+1D/2026-06-14/1"
     mkpath(dirout_base)
@@ -151,29 +150,45 @@ function main()
     dirout_plot = joinpath(dirout_base, "plot")
     mkpath.([dirout_data,dirout_plot])
     # ----------------- Parameters ------------------
-    kmax = 16 # maximum wavenumber to retain 
+    kmax = 8 # maximum wavenumber to retain 
     ks = collect(0:1:kmax)
     # ----------------- Simulation parameters -------
     dt = 0.01
-    # -----------------------------------------------
+    # ----------------- Ensemble parameters ---------
+    duration_spinup = 15.0
+    duration_spinon = 6.0 
     # ----------------- spinup --------------
-    rng = Random.MersenneTwister(48401)
-    init_wavenumbers = [1,2,4,8]
-    init_amplitudes = [0.5,0.3,0.1,0.05]
-    init_phases = 2pi .* rand(rng, Float64, length(init_amplitudes)) 
-    u_init = zeros(ComplexF64, (kmax+1,))
-    for ik = 1:length(init_wavenumbers)
-        u_init[init_wavenumbers[ik]+1] = init_amplitudes[ik] * exp(1im * init_phases[ik])
+    if todo["spinup"]
+        rng = Random.MersenneTwister(48401)
+        init_wavenumbers = [1,2] #,4,8]
+        init_amplitudes = [0.5,0.3] #,0.1,0.05]
+        init_phases = 2pi .* rand(rng, Float64, length(init_amplitudes)) 
+        u_init = zeros(ComplexF64, (kmax+1,))
+        for ik = 1:length(init_wavenumbers)
+            u_init[init_wavenumbers[ik]+1] = init_amplitudes[ik] * exp(1im * init_phases[ik])
+        end
+        u_init[1] = 1/3 
+        t_init = 0.0
+        Nt_spinup = round(Int64, duration_spinup/dt)
+        uhist_spinup,thist_spinup = integrate_tbh(u_init, t_init, dt, Nt_spinup)
+        jldopen(joinpath(dirout_data,"spinup.jld2"), "w") do f
+            f["uhist"] = uhist_spinup
+            f["thist"] = thist_spinup
+        end
     end
-    t_init = 0.0
-    Nt_spinup = round(Int64, 5/dt)
-    uhist_spinup,thist_spinup = integrate_tbh(u_init, t_init, dt, Nt_spinup)
-    jldopen(joinpath(dirout_data,"spinup.jld2"), "w") do f
-        f["uhist"] = uhist_spinup
-        f["thist"] = thist_spinup
+    if todo["plot_spinup"]
+        uhist,thist = jldopen(joinpath(dirout_data,"spinup.jld2"),"r") do f
+            f["uhist"], f["thist"]
+        end
+        plot_tbh_hov_trace(uhist, thist, joinpath(dirout_plot, "spinup.png"))
     end
+
+    #if todo["spinoff"]
+    #    u_init = uhist[:,end]
+    #end
+    return uhist, thist
 end
 
-main()
+uhist, thist = main()
 
 
